c0d41bc71b75ae3aa9203d30bdc78b9685d6a891
simonppg <simonppg@gmail.com>
2020-10-03 13:28:34 -0700

    Add GET "/" endpoint

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hi this is a test')
+const express = require('express')
+const app = express()
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+ 
+app.listen(3000)
+
+console.log('Running server on http://localhost:3000')
